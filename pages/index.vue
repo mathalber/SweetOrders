da84dc79ceb027cce1878bcf67e37b7b6c616416
mathalber <mathalber@outlook.com>
2025-08-26 10:12:06 -0300

    [FEAT] Created base project in vue, nuxt and shadcn

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+</script>
+
+<template>
+	<div class="flex flex-col items-center justify-center min-h-screen">
+        <Button  >
+          CLICA
+        </Button>
+	</div>
+</template>
